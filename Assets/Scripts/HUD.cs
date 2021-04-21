dd24f37b18ba41a4765dca788926efa725428767
smpy3468 <smpy3468@gmail.com>
2021-04-21 14:49:33 +0800

    撞到扣分、0分時死亡

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -3,39 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HUD : MonoBehaviour {
+public class HUD : MonoBehaviour
+{
 
 
-	public Text scoreText;
-	private int score;
+    public Text scoreText;
+    private int score;
 
     public bool playerDead;
 
-	// Use this for initialization
-	void Start () {
-		score = 0;
+    // Use this for initialization
+    void Start()
+    {
+        score = 0;
         playerDead = false;
-		UpdateScore ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        UpdateScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (score < 0)
+        {
+            score = 0;
+            playerDead = true;
+
+
+            GameObject.Find("Canvas").GetComponent<Canvas>().enabled = true;
+            Time.timeScale = 0;
+            UpdateScore();
+        }
 
         if (playerDead == false)
         {
             score++;
+
             UpdateScore();
         }
-	}
+    }
 
-	public void AddScore (int newScoreValue)
-	{
-		score += newScoreValue;
-		UpdateScore ();
-	}
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
 
-	void UpdateScore ()
-	{
-		scoreText.text = "Score: " + score;
-	}
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,220 +6,256 @@ using System.IO;
 using UnityEngine.SceneManagement;
 
 [Serializable]
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public HUD hud;
 
-	private float moveSpeed = 6.0f;
-	private float jumpHeight = 10.0f;
+    private float moveSpeed = 6.0f;
+    private float jumpHeight = 10.0f;
 
-	private int actualSprite = 0;
-	private float spriteInterval = 0.1f;
-	private Sprite[] sprites = new Sprite[2];
-	private Sprite[] crouchingSprites = new Sprite[2];
+    private int actualSprite = 0;
+    private float spriteInterval = 0.1f;
+    private Sprite[] sprites = new Sprite[2];
+    private Sprite[] crouchingSprites = new Sprite[2];
 
-	private String genomeBasePath = "Genomes/genome_";
+    private String genomeBasePath = "Genomes/genome_";
 
-	[SerializeField]
-	private List<Cactus> cactus = new List<Cactus> ();
+    [SerializeField]
+    private List<Cactus> cactus = new List<Cactus>();
 
-	[SerializeField]
-	private List<Jumped> jumps = new List<Jumped>();
+    [SerializeField]
+    private List<Jumped> jumps = new List<Jumped>();
 
-	private List<Genome> genomes = Utils.loadAllGenomes ();
+    private List<Genome> genomes = Utils.loadAllGenomes();
 
-	private bool isGrounded = true;
-	private bool isCrouching = false;
+    private bool isGrounded = true;
+    private bool isCrouching = false;
 
-	private int actualJumpGenome = 0;
+    private int actualJumpGenome = 0;
 
-	private bool isLearning = true; //If a real player is playing the game
+    private bool isLearning = true; //If a real player is playing the game
 
     int genomesMax = 10000;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
         hud.playerDead = false;
 
-		GetComponent<BoxCollider2D> ().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
 
-		isLearning = (genomes.Count < genomesMax);
-		if (!isLearning && Utils.actualGenome >= genomes.Count) {
-			print ("Acabou de jogar os genomas");
-			Utils.clearCrossOversFolder();
-			genomes = Utils.loadAllGenomes (); //Force GENOMES root folder
+        isLearning = (genomes.Count < genomesMax);
+        if (!isLearning && Utils.actualGenome >= genomes.Count)
+        {
+            print("Acabou de jogar os genomas");
+            Utils.clearCrossOversFolder();
+            genomes = Utils.loadAllGenomes(); //Force GENOMES root folder
 
-			List<Genome> bestGenomes = Utils.naturalSelection (genomes, genomesMax);
-			Utils.clearGenomesFolder();
+            List<Genome> bestGenomes = Utils.naturalSelection(genomes, genomesMax);
+            Utils.clearGenomesFolder();
 
-			for (int i = 0; i < bestGenomes.Count; i++) {
-				Utils.persistInJson (bestGenomes[i], genomeBasePath + i + "_");
-			}
-				
-			//New Crossovers + Mutations
-			for (int i = 0; i < genomesMax; i++) {
-				for (int j = 0; j < genomesMax; j++) {
-					if (i == j) {
-						continue;
-					}						
+            for (int i = 0; i < bestGenomes.Count; i++)
+            {
+                Utils.persistInJson(bestGenomes[i], genomeBasePath + i + "_");
+            }
 
-					Genome g = Genetic.crossOver (bestGenomes [i], bestGenomes [j]);
-					g = Genetic.mutate (g);
-					Utils.persistInJson (g, "Genomes/CrossedOvers/genome_" + i + "_" + j + "_");
-				}
-			}
+            //New Crossovers + Mutations
+            for (int i = 0; i < genomesMax; i++)
+            {
+                for (int j = 0; j < genomesMax; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
 
-			genomes = Utils.loadAllGenomes ();
-			Utils.actualGenome = 0;
-		}
-		sprites = Resources.LoadAll<Sprite> ("Art/Player/Standing");
-		crouchingSprites = Resources.LoadAll<Sprite> ("Art/Player/Crouching");
+                    Genome g = Genetic.crossOver(bestGenomes[i], bestGenomes[j]);
+                    g = Genetic.mutate(g);
+                    Utils.persistInJson(g, "Genomes/CrossedOvers/genome_" + i + "_" + j + "_");
+                }
+            }
 
-		GameObject.Find ("Canvas").GetComponent<Canvas> ().enabled = false;
+            genomes = Utils.loadAllGenomes();
+            Utils.actualGenome = 0;
+        }
+        sprites = Resources.LoadAll<Sprite>("Art/Player/Standing");
+        crouchingSprites = Resources.LoadAll<Sprite>("Art/Player/Crouching");
 
-		//Load Cactus
-		foreach(GameObject c in GameObject.FindGameObjectsWithTag("cactus"))
-		{			
-			int cacType;
-			switch (c.name) {
-				case "cactus_1":
-					cacType = 1;
-					break;
-				case "cactus_2":
-					cacType = 2;
-					break;
-				case "cactus_3":
-					cacType = 3;
-					break;
-				default:
-					cacType = 1;
-					break;
-			}
-			Cactus toAdd = new Cactus () {
-				type = cacType,
-				position = c.transform.position
-			};
-			cactus.Add (toAdd);
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {		
-		spriteInterval -= Time.deltaTime;
-		if (spriteInterval < 0) {
-			spriteInterval = 0.1f;
-			if (isCrouching) {				
-				GetComponent<SpriteRenderer> ().sprite = crouchingSprites [actualSprite];
-			} else {
-				GetComponent<SpriteRenderer> ().sprite = sprites [actualSprite];
-			}
-			actualSprite = 1 - actualSprite;
-		}
+        GameObject.Find("Canvas").GetComponent<Canvas>().enabled = false;
 
-		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+        //Load Cactus
+        foreach (GameObject c in GameObject.FindGameObjectsWithTag("cactus"))
+        {
+            int cacType;
+            switch (c.name)
+            {
+                case "cactus_1":
+                    cacType = 1;
+                    break;
+                case "cactus_2":
+                    cacType = 2;
+                    break;
+                case "cactus_3":
+                    cacType = 3;
+                    break;
+                default:
+                    cacType = 1;
+                    break;
+            }
+            Cactus toAdd = new Cactus()
+            {
+                type = cacType,
+                position = c.transform.position
+            };
+            cactus.Add(toAdd);
+        }
+    }
 
-		if (isLearning) {
-			if (Input.GetKeyUp (KeyCode.DownArrow)) {
-				isCrouching = false;
-				GetComponent<PolygonCollider2D> ().enabled = true;
-				GetComponent<BoxCollider2D> ().enabled = false;
-			}
-				
-			if (isGrounded && (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.UpArrow))) {
-				isCrouching = false;
-				GetComponent<PolygonCollider2D> ().enabled = true;
-				GetComponent<BoxCollider2D> ().enabled = false;
+    // Update is called once per frame
+    void Update()
+    {
+        spriteInterval -= Time.deltaTime;
+        if (spriteInterval < 0)
+        {
+            spriteInterval = 0.1f;
+            if (isCrouching)
+            {
+                GetComponent<SpriteRenderer>().sprite = crouchingSprites[actualSprite];
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().sprite = sprites[actualSprite];
+            }
+            actualSprite = 1 - actualSprite;
+        }
 
-				GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
-				isGrounded = false;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
-				Cactus c = getNextNearestCactus ();
-				Jumped jump = new Jumped {
-					nearestCactus = c,
-					distanceToNearestCactus = c.position - GetComponent<Rigidbody2D> ().position
-				};
+        if (isLearning)
+        {
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                isCrouching = false;
+                GetComponent<PolygonCollider2D>().enabled = true;
+                GetComponent<BoxCollider2D>().enabled = false;
+            }
 
-				jumps.Add (jump);
-			} else if (isGrounded && !isCrouching && Input.GetKeyDown (KeyCode.DownArrow)) {
-				isCrouching = true;
-				GetComponent<BoxCollider2D> ().enabled = true;
-				GetComponent<PolygonCollider2D> ().enabled = false;
-			}
-		} else {
-			if(Time.timeScale != 0) {
-				if (Utils.actualGenome < genomes.Count) {
-					playGenome(Utils.actualGenome);	
-				}					
-			}
-		}
-	}
+            if (isGrounded && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)))
+            {
+                isCrouching = false;
+                GetComponent<PolygonCollider2D>().enabled = true;
+                GetComponent<BoxCollider2D>().enabled = false;
 
-	// Called when a collision happens
-	void OnCollisionEnter2D(Collision2D coll) {
-		if (coll.gameObject.name.StartsWith ("cactus")) {
-            hud.playerDead = true;
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+                isGrounded = false;
 
-            GameObject.Find ("Canvas").GetComponent<Canvas> ().enabled = true;
-			Time.timeScale = 0;
+                Cactus c = getNextNearestCactus();
+                Jumped jump = new Jumped
+                {
+                    nearestCactus = c,
+                    distanceToNearestCactus = c.position - GetComponent<Rigidbody2D>().position
+                };
 
-			Genome genome = new Genome {
-				fitness = Genetic.calculateFitness(jumps, cactus),
-				jumps = jumps
-			};
+                jumps.Add(jump);
+            }
+            else if (isGrounded && !isCrouching && Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                isCrouching = true;
+                GetComponent<BoxCollider2D>().enabled = true;
+                GetComponent<PolygonCollider2D>().enabled = false;
+            }
+        }
+        else
+        {
+            if (Time.timeScale != 0)
+            {
+                if (Utils.actualGenome < genomes.Count)
+                {
+                    playGenome(Utils.actualGenome);
+                }
+            }
+        }
+    }
 
-			Utils.persistInJson (genome, genomeBasePath);
+    // Called when a collision happens
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name.StartsWith("cactus"))
+        {
+            //hud.playerDead = true;
+            hud.AddScore(-300);
 
-			jumps.Clear();
+            //Genome genome = new Genome
+            //{
+            //    fitness = Genetic.calculateFitness(jumps, cactus),
+            //    jumps = jumps
+            //};
 
-			if(!isLearning) {
-				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-				Time.timeScale = 1;
-				Utils.actualGenome++;
-			}				
-		} else if (coll.gameObject.name.StartsWith ("Ground")) {
-			isGrounded = true;
-		}
+            //Utils.persistInJson(genome, genomeBasePath);
 
-        
-	}
+            //jumps.Clear();
 
-	Cactus getNextNearestCactus() {
-		float nearestDist = float.PositiveInfinity;
-		Cactus nearestCactus = null;
-		foreach(Cactus c in cactus)
-		{
-			float cacX = c.position.x;
-			float playerX = GetComponent<Rigidbody2D> ().position.x;
-			if (cacX > playerX) {
-				float dist = cacX - playerX;
-				if (dist < nearestDist) {
-					nearestDist = dist;
-					nearestCactus = c;
-				}
-			}
-		}
+            //if (!isLearning)
+            //{
+            //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            //    Time.timeScale = 1;
+            //    Utils.actualGenome++;
+            //}
+            Destroy( coll.gameObject );
+        }
+        else if (coll.gameObject.name.StartsWith("Ground"))
+        {
+            isGrounded = true;
+        }
 
-		return nearestCactus;
-	}
 
-	void playGenome(int genomeIndex) {
-		float dist = getNextNearestCactus().position.x - GetComponent<Rigidbody2D> ().position.x;
-		if (actualJumpGenome >= genomes[genomeIndex].jumps.Count) {
-		}
-		else if (dist <= genomes[genomeIndex].jumps[actualJumpGenome].distanceToNearestCactus.x && isGrounded) {
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
-			isGrounded = false;
+    }
 
-			Cactus c = getNextNearestCactus ();
-			Jumped jump = new Jumped {
-				nearestCactus = c,
-				distanceToNearestCactus = c.position - GetComponent<Rigidbody2D> ().position
-			};
+    Cactus getNextNearestCactus()
+    {
+        float nearestDist = float.PositiveInfinity;
+        Cactus nearestCactus = null;
+        foreach (Cactus c in cactus)
+        {
+            float cacX = c.position.x;
+            float playerX = GetComponent<Rigidbody2D>().position.x;
+            if (cacX > playerX)
+            {
+                float dist = cacX - playerX;
+                if (dist < nearestDist)
+                {
+                    nearestDist = dist;
+                    nearestCactus = c;
+                }
+            }
+        }
 
-			jumps.Add (jump);
+        return nearestCactus;
+    }
 
-			actualJumpGenome++;
-		}
-	}
+    void playGenome(int genomeIndex)
+    {
+        float dist = getNextNearestCactus().position.x - GetComponent<Rigidbody2D>().position.x;
+        if (actualJumpGenome >= genomes[genomeIndex].jumps.Count)
+        {
+        }
+        else if (dist <= genomes[genomeIndex].jumps[actualJumpGenome].distanceToNearestCactus.x && isGrounded)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+            isGrounded = false;
+
+            Cactus c = getNextNearestCactus();
+            Jumped jump = new Jumped
+            {
+                nearestCactus = c,
+                distanceToNearestCactus = c.position - GetComponent<Rigidbody2D>().position
+            };
+
+            jumps.Add(jump);
+
+            actualJumpGenome++;
+        }
+    }
 }
